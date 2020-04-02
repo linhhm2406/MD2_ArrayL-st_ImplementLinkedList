50f18bd3a3fb5866597d93a55841fbae65632250
linhhm2406 <linh.hm24@gmail.com>
2020-04-02 22:41:54 +0700

    add file

---
+++ src/MyListTest.java
@@ -0,0 +1,17 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        System.out.println("Test is below: ");
+        MyList<Integer> myList = new MyList<>(10);
+        myList.add(1, 10);
+        myList.add(2, 20);
+        myList.add(3, 30);
+
+        System.out.println(myList.size());
+        System.out.println(myList.contains(30));
+        System.out.println(myList.indexOf(10));
+        System.out.println(myList.get(2));
+        System.out.println(myList.getFirst());
+        System.out.println(myList.getLast());
+
+    }
+}
